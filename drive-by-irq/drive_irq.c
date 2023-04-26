738e9ebdbc8815821bb04a25868d0ca3eaef0113
Graeme Winter <graeme.winter@gmail.com>
2023-04-26 07:35:18 +0100

    IRQ override works

--- drive-by-irq/drive_irq.c
+++ drive-by-irq/drive_irq.c
@@ -3,6 +3,7 @@
 // defines
 #define EIC_BASE 0x40002800
 #define VTOR_ADDR 0xe000ed08
+#define PORT_BASE 0x41008000
 
 // memory structure - interrupt vector is length > 128 words -> 256 word alignment
 // needed so just allocate 2 kB of memory, seek to point where this is aligned
@@ -13,12 +14,10 @@ unsigned int *irq_vector_copy = NULL;
 unsigned int *VTOR_INIT = 0;
 
 void irq_action(void) {
-  // toggle PA22
-  unsigned int *PA_OUTTGL = (unsigned int *) (0x41008000 | 0x1c);
-  unsigned int LED_BIT = 0x1 << 22;
-  *PA_OUTTGL = LED_BIT;
+  // clear PA22
+  *((unsigned int *) (PORT_BASE | 0x14)) = 0x1 << 22;
 
-  // clear flag
+  // clear IRQ
   *(unsigned int *) (EIC_BASE | 0x14) = 0x1 << 7;
 }
 
@@ -41,8 +40,6 @@ STATIC mp_obj_t drive_irq_init(void) {
     // Update VTOR
     *(unsigned int **)VTOR_ADDR = irq_vector_copy;
 
-    // FIXME enable IRQ
-    
   }
 
   mp_int_t result = (mp_int_t)VTOR_INIT;
@@ -51,8 +48,6 @@ STATIC mp_obj_t drive_irq_init(void) {
 
 STATIC mp_obj_t drive_irq_deinit(void) {
   if (buffer) {
-    // FIXME disable IRQ
-
     // Revert VTOR
     *(unsigned int **)VTOR_ADDR = VTOR_INIT;
 
@@ -65,17 +60,8 @@ STATIC mp_obj_t drive_irq_deinit(void) {
   return mp_obj_new_int(result);
 }
 
-STATIC mp_obj_t drive_irq_blink(void) {
-  for (int j = 0; j < 1000000; j++) {
-    irq_action();
-  }
-  mp_int_t result = (mp_int_t) 0;
-  return mp_obj_new_int(result);
-}
-
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(drive_irq_init_obj, drive_irq_init);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(drive_irq_deinit_obj, drive_irq_deinit);
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(drive_irq_blink_obj, drive_irq_blink);
 
 mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw,
                   mp_obj_t *args) {
@@ -83,7 +69,6 @@ mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw,
 
   mp_store_global(MP_QSTR_init, MP_OBJ_FROM_PTR(&drive_irq_init_obj));
   mp_store_global(MP_QSTR_deinit, MP_OBJ_FROM_PTR(&drive_irq_deinit_obj));
-  mp_store_global(MP_QSTR_blink, MP_OBJ_FROM_PTR(&drive_irq_blink_obj));
 
   MP_DYNRUNTIME_INIT_EXIT
 }
