78ad7e813b21b291ce72c8b549cfadd611101988
Graeme Winter <graeme.winter@gmail.com>
2023-05-08 09:24:08 +0100

    WIP: UART controllers

---
+++ samd51-spot-finding/uart_driver.py
@@ -0,0 +1,41 @@
+from machine import Pin, UART
+import time
+
+import spot_filter
+
+
+def main():
+
+    spot_filter.init(512, 1028)
+
+    uart = UART(1, 115200 * 8, rx=Pin("D1"), tx=Pin("D0"))
+
+    led = Pin("D13", Pin.OUT)
+
+    buffer = bytearray(1028 * 2)
+
+    frame = 0
+
+    while True:
+        t0 = time.ticks_ms()
+        signal = 0
+        for i in range(18):
+            for j in range(512):
+                uart.readinto(buffer, 1028 * 2)
+                signal += spot_filter.row(buffer)
+                if j >= 3:
+                    uart.write(buffer)
+            # last 3 rows
+            for j in range(3):
+                signal += spot_filter.row(buffer)
+                uart.write(buffer)
+            led.toggle()
+
+        t1 = time.ticks_ms()
+
+        print(f"Frame {frame} => {signal} signal pixels in {0.001*(t1 - t0):.2f}s")
+
+    spot_filter.deinit()
+
+
+main()
