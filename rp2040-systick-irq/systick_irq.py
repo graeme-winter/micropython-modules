ef95456517bafd6dc8a29b5fe05812b459307924
Graeme Winter <graeme.winter@gmail.com>
2023-08-30 13:58:17 +0100

    M0 systick - count zero interrupt (#4)
    Basic impl
    
    Stash systick on deinit, print

---
+++ rp2040-systick-irq/systick_irq.py
@@ -0,0 +1,23 @@
+from machine import mem32, Pin
+import time
+
+import systick
+
+led = Pin(25, Pin.OUT)
+
+# enable interrupt handler
+systick.init()
+
+PPB_BASE = 0xE0000000
+SYST_CSR = PPB_BASE | 0xE010
+SYST_RVR = PPB_BASE | 0xE014
+SYST_CVR = PPB_BASE | 0xE018
+
+mem32[SYST_RVR] = 12_500_000
+
+mem32[SYST_CSR] = 0x7
+time.sleep(10)
+mem32[SYST_CSR] = 0x0
+
+# print number of cycles needed on last delay
+print(12_500_000 - systick.deinit())
