fe38b1527337d1a36c1c9a593572c456c3ec1d0c
Graeme Winter <graeme.winter@gmail.com>
2023-08-30 19:19:05 +0100

    Add get()

--- rp2040-systick-irq/systick.c
+++ rp2040-systick-irq/systick.c
@@ -2,7 +2,7 @@
 
 unsigned int original_handler;
 unsigned int original_handler_set;
-unsigned int systick;
+volatile unsigned int systick;
 
 #define VTOR_ADDR 0xe000ed08
 #define SYST_CVR 0xe000e018
@@ -34,8 +34,13 @@ STATIC mp_obj_t irq_deinit(void) {
   return mp_obj_new_int(systick);
 }
 
+STATIC mp_obj_t irq_get(void) {
+  return mp_obj_new_int(systick);
+}
+
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(irq_init_obj, irq_init);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(irq_deinit_obj, irq_deinit);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(irq_get_obj, irq_get);
 
 mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw,
                   mp_obj_t *args) {
@@ -45,6 +50,7 @@ mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw,
 
   mp_store_global(MP_QSTR_init, MP_OBJ_FROM_PTR(&irq_init_obj));
   mp_store_global(MP_QSTR_deinit, MP_OBJ_FROM_PTR(&irq_deinit_obj));
+  mp_store_global(MP_QSTR_get, MP_OBJ_FROM_PTR(&irq_get_obj));
 
   MP_DYNRUNTIME_INIT_EXIT
 }
